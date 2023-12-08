2fa561b5ce0dbf9c012900868e55cbf0cf15ba51
carolineking227 <111394942+carolineking227@users.noreply.github.com>
2023-12-07 20:09:41 -0500

    ok

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,45 +1,27 @@
-"""Ex01 - Chardle"""
+"""Ex01 - Chardle."""
     
-__author__="730494174"
+__author__ = "730494174"
 
-chosen_word: str = input("Enter a 5-character word: ")
-if len(chosen_word) !=5:
+chosen_word = input("Enter a 5-character word: ")
+if len(chosen_word) != 5:
     print("Error: Word must contain 5 characters")
     exit()
-chosen_letter: str = input("Enter a single character: ")
-if len(chosen_letter) !=1:
+chosen_letter = input("Enter a single character: ")
+if len(chosen_letter) != 1:
     print("Error: Character must be a single character.")
     exit()
-print("Searching for " + letter + " in " + chosen_word)
+print("Searching for {chosen_letter} in {chosen_word}")
 
-pos_0 = (chosen_word[0])
-pos_1 = (chosen_word[1])
-pos_2 = (chosen_word[2])
-pos_3 = (chosen_word[3])
-pos_4 = (chosen_word[4])
+count = 0
 
-instance: int = 0
+for i in range(len(chosen_word)):
+    if chosen_word[i] == chosen_letter:
+        print(f"{chosen_letter} found at index {i}")
+        count += 1
 
-if pos_0 == chosen_letter:
-    print(chosen_letter + " found at index 0 ")
-    instance = instance + 1
-if pos_1 == chosen_letter :
-    print(chosen_letter + " found at index 1 ")
-    instance = instance + 1
-if pos_2 == chosen_letter :
-    print(chosen_letter + " found at index 2 ")
-    instance = instance + 1
-if pos_3 == chosen_letter :
-    print(chosen_letter + " found at index 3 ")
-    instance = instance + 1
-if pos_4 == chosen_letter :
-    print(chosen_letter + " found at index 4 ")
-    instance = instance + 1
-
-
-if instance == 0:
-    print("No instances of " + chosen_letter + " found in " + chosen_word)
-if instance == 1:
-    print("1 instance of " + chosen_letter + " found in " + chosen_word)
-else: 
-    print(str(instance) + " instances of " + chosen_letter + " found in " + chosen_word)
+if count == 0:
+    print("No instances of {chosen_letter} in {chosen_word}")
+elif count == 1:
+    print("1 instance of {chosen_letter} found in {chosen_word}")
+else:
+    print("{count} instances of {chosen_letter} in {chosen_word}")
